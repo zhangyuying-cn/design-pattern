c22024a6c884ac3a430197a12e3f4178d2e2a4dc
zhangyuying <zhangyuying@thunisoft.com>
2018-08-16 13:34:48 +0800

    fix:去掉没有用到的代码

--- simple-factory/src/main/java/cn/zhangyuying/designpattern/simplefactory/SendType.java
+++ simple-factory/src/main/java/cn/zhangyuying/designpattern/simplefactory/SendType.java
@@ -20,41 +20,25 @@ public enum SendType {
     SMS(3, "短信消息");
 
     /**
-     * 模式的值
+     * 类型的值
      */
     private int value = 0;
 
     /**
-     * 模式的描述信息
+     * 类型的描述信息
      */
     private String describe;
 
     /**
      * 构造函数
      *
-     * @param value 模式值
-     * @param describe 模式描述
+     * @param value 类型值
+     * @param describe 类型描述
      */
     SendType(int value, String describe) {
         this.value = value;
         this.describe = describe;
     }
 
-    /**
-     * 获取类型值
-     *
-     * @return 模式值
-     */
-    public int getValue() {
-        return value;
-    }
 
-    /**
-     * 获取类型描述信息
-     *
-     * @return 模式描述信息
-     */
-    public String getDescribe() {
-        return describe;
-    }
 }
